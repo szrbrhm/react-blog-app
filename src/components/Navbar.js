d1c96dee93263ca09fc960f5ab9f2fec67e47639
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-22 09:37:10 +0200

    add

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,15 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-
-import AccountCircle from '@mui/icons-material/AccountCircle';
-
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
 
 export default function MenuAppBar() {
-  
   const [anchorEl, setAnchorEl] = React.useState(null);
-
-
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
@@ -29,47 +24,55 @@ export default function MenuAppBar() {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          <a href="https://clarusway.com/">
-              <img src="https://eds-fireblog.herokuapp.com/static/media/cw.041cf5e8.jpeg" alt="" style={{ width: "40px", height:"40px" }}/>
+            <a href="https://clarusway.com/">
+              <img
+                src="https://eds-fireblog.herokuapp.com/static/media/cw.041cf5e8.jpeg"
+                alt=""
+                style={{ width: "40px", height: "40px" }}
+              />
             </a>
           </Typography>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-           <a href="https://github.com/szrbrhm" target="_blank" rel="noreferrer" style={{textDecoration:"none", color:"white"}}>
-            <i>{"< SzrBrhm / > BLOG"} </i>
+            <a
+              href="https://github.com/szrbrhm"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              <i>{"< SzrBrhm / > BLOG"} </i>
             </a>
           </Typography>
-          
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Login</MenuItem>
-                <MenuItem onClick={handleClose}>Register</MenuItem>
-              </Menu>
-            </div>
-          
+
+          <div>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Login</MenuItem>
+              <MenuItem onClick={handleClose}>Register</MenuItem>
+            </Menu>
+          </div>
         </Toolbar>
       </AppBar>
     </Box>
