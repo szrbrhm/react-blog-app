00a69c0a778175b255cd2572f7b939f2f7221d83
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-24 23:35:41 +0200

    add ıcon

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,15 +1,19 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import IconButton from "@mui/material/IconButton";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 export default function ImgMediaCard() {
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardMedia 
+      <CardMedia
         component="img"
         alt="green iguana"
         height="140"
@@ -20,14 +24,26 @@ export default function ImgMediaCard() {
           HTML
         </Typography>
         <Typography variant="body2" color="text.secondary">
-        The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.
+          The HyperText Markup Language, or HTML is the standard markup language
+          for documents designed to be displayed in a web browser. It can be
+          assisted by technologies such as Cascading Style Sheets and scripting
+          languages such as JavaScript.
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        style={{ fontWeight: "bold" }}
+      >
+        <AccountCircleIcon style={{ marginRight: "5px", marginLeft: "10px" }} />
+        kapadokya@gmail.com
+      </Typography>
+      <IconButton>
+        <FavoriteBorderIcon />
+      </IconButton>
+      <IconButton>
+        <ChatBubbleOutlineIcon />
+      </IconButton>
     </Card>
-    
   );
 }
