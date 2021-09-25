e323e6237530e037a0f75bcb2bc59914fd87d464
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-25 14:54:10 +0200

    create Header Comp

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BlogLogo from '../img/blok.7e6674a5.png'
+
+const Header = () => {
+    return (
+        <div style={{textAlign:"center"}}>
+            <h2 style={{fontSize:"50px", color:"blue", fontWeight:"bold"}}>──── DashBoard ────</h2>
+        </div>
+    )
+}
+
+export default Header
