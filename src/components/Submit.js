3ea36e817d439272346e04730e7df35152de76cb
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-25 14:54:22 +0200

    Create Submit Comp

---
+++ src/components/Submit.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+import SendIcon from '@mui/icons-material/Send';
+import Stack from '@mui/material/Stack';
+
+export default function IconLabelButtons() {
+  return (
+    <Stack direction="row">
+      <Button variant="contained" endIcon={<SendIcon />}>
+        Send
+      </Button>
+    </Stack>
+  );
+}
