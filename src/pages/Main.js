4219ec771e6b3ba370766cfaf97586d75ec43418
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-25 14:54:54 +0200

    add

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Card from '../components/Card'
+import Header from '../components/Header'
+import FormPropsTextFields from '../pages/New'
 
 const Main = () => {
   return (
     <div>
-      <Card/>
+      {/* <Card/> */}
+      {/* <Header/> */}
+      <FormPropsTextFields/>
     </div>
   )
 }
