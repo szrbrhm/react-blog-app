7d0e7cc8279600ea59a68f7ade711688c611df94
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-27 18:38:27 +0200

    add new pages

--- src/router/Router.js
+++ src/router/Router.js
@@ -3,6 +3,7 @@ import Main from '../pages/Main'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
 import Navbar from '../components/Navbar'
+import New from '../pages/New'
 
 const AppRouter = () => {
     return(
@@ -11,6 +12,7 @@ const AppRouter = () => {
             <Switch>
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/login" component={Login} />
+                <Route exact path="/new" component={New} />
                 <Route exact path="/" component={Main} />
             </Switch>
         </Router>
