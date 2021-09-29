5de8e420d6de604edcc0f3bbfe4beeb321d08ae5
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-29 23:04:27 +0200

    add pages

---
+++ src/pages/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Profile = () => {
+    return (
+        <div>
+            <h1>Display Name</h1>
+            <span></span>
+            <h1>Email</h1>
+            <span></span>
+        </div>
+    )
+}
+
+export default Profile
